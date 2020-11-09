6df5f0b7a4ff1146914c7e1dfcaa5ae077383bd1
Laura Toft <laur507p@stud.kea.dk>
2020-11-09 12:56:04 +0100

    form virker  ikke lige nu hehe

--- form.js
+++ form.js
@@ -2,29 +2,62 @@
 
 const form = document.querySelector("form");
 const formElements = form.elements;
+// const submit = formElements[7];
 
-const fornavn = formElements[0].value;
-const efternavn = formElements[1].value;
-const arbejdsmail = formElements[2].value;
+const fornavn = formElements.name.value;
+const efternavn = formElements.lastname.value;
+const arbejdsmail = formElements.email.value;
+const virksomhed = formElements.company.value;
+const jobtitel = formElements.job.value;
+const land = formElements.country.value;
 
 const key = "5f96ab834b77c1637d147e00";
 const endpoint = "https://frontend2020-18a2.restdb.io/rest/dxc-signups";
 
-console.log(formElements);
+window.addEventListener("load", start);
 
-form.setAttribute("novalidate", true);
+function start() {
+  form.setAttribute("novalidate", true);
 
-// function that posts data to restdb
-function post() {
-  const data = {
-    fornavn: "",
-    efternavn: "",
-    arbejdsmail: "",
-    virksomhed: "",
-    jobtitel: "",
-    land: "",
+  form.addEventListener("submit", checkForm);
+}
+
+function checkForm() {
+  console.log("checkform");
+  const formIsValid = form.checkValidity();
+
+  if (formIsValid) {
+    console.log("form is valid");
+    // submitForm();
+  } else {
+    console.log("form is invalid");
+    formElements.forEach((elm) => {
+      elm.setAttribute("required", true);
+    });
+  }
+}
+
+function submitForm() {
+  console.log("submit form");
+
+  form.preventDefault();
+
+  form.elements.submit.disabled = true;
+
+  const myData = {
+    fornavn: formElements.name.value,
+    efternavn: formElements.lastname.value,
+    arbejdsmail: formElements.email.value,
+    virksomhed: formElements.company.value,
+    jobtitel: formElements.job.value,
+    land: formElements.country.value,
   };
 
+  post(myData);
+}
+
+// function that posts data to restdb
+function post(data) {
   const postData = JSON.stringify(data);
   fetch(endpoint, {
     method: "post",
