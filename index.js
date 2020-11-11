935ca5d0a9d6f62a0497a831a6dbb21d9aeaf699
Andrea Valgeirsdottir <andr60c0@stud.kea.dk>
2020-11-11 15:21:09 +0100

    video overlay knap

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 "use strict";
+
+let videobutton = document.querySelector("#videobutton");
+
+let video = document.querySelector("video");
+
+let videoOverlay = document.querySelector(".video_overlay");
+
+videobutton.addEventListener("click", playVideo);
+
+function playVideo() {
+  videoOverlay.classList.add("hide");
+  video.play();
+}
